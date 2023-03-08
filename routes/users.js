63705334a05867fb9500934f2887db0a7486e6c7
Joshua Makula <makulajosh@gmail.com>
2023-03-08 12:55:35 +0300

    Passing form/JSON DATA

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,26 @@ router.get('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-    res.send("New user Form")
+    res.render("users/new")
 })
 
+router
+.route("/:id")
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`Get user with ID ${req.params.id}` )
+    })
+    .put((req, res) => {
+        res.send(`Update user with ID ${req.params.id}` )
+    })
+    .delete((req, res) => {
+        res.send(`Delete user with ID ${req.params.id}` )
+    })
+
+    const users = [{name:"Kyle"}, {name: "Sally"}]
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express')
 // setup actual server
 const app = express()
 
+// Middleware for static files
+app.use(express.static("public"))
+
 // Tell application to use view engine/ejs
 app.set('view engine', 'ejs')
+
+app.use(logger)
 
 // setup routes
 app.get('/', (req, res) => {
@@ -18,6 +23,11 @@ const userRouter = require('./routes/users')
 
 // Link up the user routes
 app.use('/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 // make the server run
 app.listen(3000)
